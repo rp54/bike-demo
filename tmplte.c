fa2e54a8464de695bc3c77d94e9ecd8d818a30e6
rp54 <russell.potter3@gmail.com>
2014-01-13 20:26:13 +1100

    Create tmplte.c

---
+++ tmplte.c
@@ -0,0 +1,38 @@
+/****************************************************************************
+*
+* BikeDemo - Exercise bike demonstration scenery.
+*
+* lex.c - function implementations relating to 
+* "templates", part of the "Web Components"
+* suite of specifications,
+* which
+* consists of both the language's grammar productions,
+* its lexical analyser, and the resulting parsing
+* table.
+*
+* Copyright (C) 2006-2013, Russell Potter, All
+* Rights Reserved
+*
+****************************************************************************/
+
+#include <bikedemo/xml.h>
+#include <bikedemo/allocs.h>
+#include <bikedemo/log.h>
+
+/* "bdtl_actvte" - "activates" (that is, inserts
+ * the relevant node, and running the relevant
+ * "activation" call-back) the template given
+ * in "tmplte" into the document given in "doc",
+ * using the opaque parameyeter, memory allocator
+ * and error logger given in "opaque", "allocs"
+ * and "logger", respectively */
+int bdtl_actvte(struct bdtl_tmplte *tmplte,
+                struct bdxl_doc *doc,
+                void * opaque,
+                struct bd_allocs *allocs,
+                struct bd_logger *logger)
+{
+    if (!(bdxl_add_node(tmplte->node, tmplte)))
+
+    return	1;
+}
