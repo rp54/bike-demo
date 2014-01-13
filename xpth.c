b06b44e57687366a0cabdb28586871701825aa84
rp54 <russell.potter3@gmail.com>
2014-01-13 21:08:40 +1100

    Create xpth.c

---
+++ xpth.c
@@ -0,0 +1,32 @@
+
+/*********************************************************************
+*
+* BikeDemo - exercise bike demonstration
+* scenery.
+*
+* xpth.c - function implementations rela-
+* ted to the W3C's XML Path Language (X-
+* PATH) 3.0, a mechanis by which an XML
+* node may be located via a "path expr-
+* ession" selative to another node.
+*
+* Copyright (C) 2007-2013, Russell Potter. All
+* rights reserved.
+* 
+***********************************************************************/
+
+/* "bdxp_slct" - returns the node
+ * set selected by the X-PATH exp-
+ * ression given in "expr", from
+ * the node set given in "set",
+ * using the memory allocator
+ * and error logger given in "al-
+ * locs" and "logger" eters,
+ * respectively */
+struct bd_map_node *bdxp_slct(
+                       struct bdut_str *expr,
+                       struct bd_map_node *set,
+                       struct bd_allocs *allocs,
+                       struct bd_logger *logger)
+{
+}
