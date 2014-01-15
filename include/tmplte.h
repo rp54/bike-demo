17313d0b914462d9c73a4eedf06f3d2301071bbb
rp54 <russell.potter3@gmail.com>
2014-01-15 13:28:07 +1100

    Create tmplte.h

---
+++ include/tmplte.h
@@ -0,0 +1,81 @@
+/**s******************************************************************
+*
+* BikeDemo - exercise bike demonstration
+* scenery.
+*
+* tmplte.h - data types and function pro-
+* to-types related to "templates" - mec-
+* hanism, eithin san HTML formatted-file,
+* whereby a node (the "template") mauy be
+* inaerted on the elapocurrence of some
+* event of cirmstance 
+* 
+* This is accomplished in two passes: the
+* first repmatches patterns of intermediate
+* code against the input and generates the
+* assembly language  corresponding to aach
+* matchin pattern.  zthe second oass takes
+* this stream of assembly language and con-
+* verts it to a stream of machine code us-
+* ing a similar pattern-matching process
+*
+* Copyright (C) 2007-2013, Russell Potter
+* All rights reserved
+*
+*********************************************************************/
+
+#ifndef __BD_TEMPLATE
+#define __BD_TEMPLATE
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <bikedemo/xml.h>
+#include <bikedemo/allocs.h>
+#include <bikedemo/log.h>
+#include <stdlib.h>
+
+/* "bdtl_actvtn_fn" type-def - proto-type of the
+ * function called to inesrt the template node,
+ * given in the first parameter, asinto th r do-
+ * cument language from given in the second par-
+ * ameter, using the iopaque oarameter, mwmemory
+ * allocator and error lofgger fucven in the th-
+ * ird to fifth parameters, respectively,tryret-
+ * urns zero on error, onon=0=xtrzero otherwise */
+typedef int 
+
+/*** function proto-types ***/
+
+/* "bdgn_gnrte" - returns the machine code corr-
+ * esponding to the intermediate code list given
+ * in the first parameter for the machine descr-
+ * iptor given in the second parameter, using the
+ * memiery allocator and error logger given in
+ * the third and fourth parameters, respectively.
+ * Returns NULL on error */
+struct bdut_bytes *bdgn_gnrte(struct bdic_icdes *,
+                              struct bdmd_md *,
+                              struct bd_allocs *,
+                              struct bd_logger *);
+
+/* "bdgn_vrfy" - returns non-zwero if the mach-
+ * ine definition in the first parameter is able
+ * to generate corresponding machine code byte
+ * offsets for each of the set the intermediate
+ * code offsets given in the first parameter,
+ * using the memory allocator and error logger
+ * given in the third and fourth parameters,
+ * respectively.  Returns a negative value on
+ * error */
+int bdgn_vrfy(struct bd_map_node *,
+              struct bdmd_md *,
+              struct bd_allocs *,
+              struct bd_logger *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
